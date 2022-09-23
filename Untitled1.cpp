253e78c49cb01dcf95ecdb59b2b34de579e62f1c
ken22i <ken22i22i22i@gmail.com>
2022-09-23 14:12:36 +0800

    no message

---
+++ Untitled1.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+const int MAX=100;
+static int a[MAX];
+static int num=0;
+
+void gen(int i){
+
+
+     if(i==num){
+
+         for(int j=0;j<num;j++){
+         	printf("%d",a[j]);
+		 }
+         
+         printf("\n");
+
+     }
+     else{
+
+         a[i]=0;
+         gen(i+1);
+         a[i]=1;
+         gen(i+1);
+     }
+}
+
+ 
+
+int main(){
+
+     printf("Input a number:");
+     scanf("%d",&num);
+     
+     gen(0);
+     return 0;
+}
